016a27e92a81ec4e4e6c5ea689d64e38c60e87c8
Matb24 <matiasbch2408@gmail.com>
2025-06-28 12:17:22 -0500

    Implementación Backend
    Se armo el Backend para comenzar a trabajar

---
+++ backend/src/test/java/com/wikimedic/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wikimedic.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

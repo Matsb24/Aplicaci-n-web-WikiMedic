603e465323c9e0783ec646867b75a0e076a25689
Matb24 <matiasbch2408@gmail.com>
2025-06-28 23:09:12 -0500

    nuevo spring boot

---
+++ backend/src/test/java/com/WikiMedic/Backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.WikiMedic.Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
